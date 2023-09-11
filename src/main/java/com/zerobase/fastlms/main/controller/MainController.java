96153f11b2cb31a3cbabf97a45c4f29e57209e9f
Sung Hyobin <hellmir610@gmail.com>
2023-09-11 16:45:22 +0900

    feat(display): 배너 노출 기능 추가

--- src/main/java/com/zerobase/fastlms/main/controller/MainController.java
+++ src/main/java/com/zerobase/fastlms/main/controller/MainController.java
@@ -1,34 +1,38 @@
 package com.zerobase.fastlms.main.controller;
 
 
+import com.zerobase.fastlms.admin.dto.BannerDto;
+import com.zerobase.fastlms.banner.service.BannerDisplayService;
 import com.zerobase.fastlms.member.service.LoginHistoryService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class MainController {
 
     private final LoginHistoryService loginHistoryService;
+    private final BannerDisplayService bannerDisplayService;
+
 
     @RequestMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request, Model model) {
 
         String userAgent = request.getHeader("User-Agent");
         String clientIp = getClientIP(request);
 
-        log.info(userAgent);
-        log.info(clientIp);
-
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
         loginHistoryService.saveLoginHistory(username, clientIp, userAgent);
+
+        List<BannerDto> bannersToDisplay = bannerDisplayService.getBannersToDisplay();
+        model.addAttribute("banners", bannersToDisplay);
 
         return "index";
 
